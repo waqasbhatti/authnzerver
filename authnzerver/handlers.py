0a657231da708e8e68dfae57c0760b42f71d1822
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2020-04-05 19:22:44 -0400

    [WIP] lock user after 10 attempts, schedule unlock, TODO: tests

--- authnzerver/handlers.py
+++ authnzerver/handlers.py
@@ -314,9 +314,10 @@ class AuthHandler(tornado.web.RequestHandler):
             self.executor,
             actions.get_user_by_email,
             {'email':payload['body']['email'],
-             'reqid':payload['reqid'],
+             'reqid':payload['body']['reqid'],
              'pii_salt':payload['body']['pii_salt']}
         )
+
         if not user_info['success']:
 
             LOGGER.error(
@@ -325,44 +326,48 @@ class AuthHandler(tornado.web.RequestHandler):
                 pii_hash(payload['body']['email'], payload['body']['pii_salt'])
             )
 
-        # attempt to lock the user using actions.internal_toggle_user_lock
-        locked_info = await loop.run_executor(
-            actions.internal_toggle_user_lock,
-            {'target_userid':user_info['user_id'],
-             'action':'lock',
-             'reqid':payload['body']['reqid'],
-             'pii_salt':payload['body']['pii_salt']}
-        )
+        else:
 
-        if locked_info['success']:
-
-            unlock_after_dt = (datetime.utcnow() +
-                               timedelta(seconds=unlock_after_seconds))
-
-            # schedule the unlock
-            loop.call_later(
-                unlock_after_seconds,
-                self.scheduled_user_unlock,
-                user_info['user_id'],
-                payload['body']['reqid'],
-                payload['body']['pii_salt']
+            # attempt to lock the user using actions.internal_toggle_user_lock
+            locked_info = await loop.run_executor(
+                actions.internal_toggle_user_lock,
+                {'target_userid':user_info['user_info']['user_id'],
+                 'action':'lock',
+                 'reqid':payload['body']['reqid'],
+                 'pii_salt':payload['body']['pii_salt']}
             )
 
-            LOGGER.warning(
-                "Locked the account for user ID: %s, email: %s after repeated "
-                "failed login attempts. Unlock scheduled for: %sZ" %
-                (pii_hash(user_info['user_id'],
-                          payload['body']['pii_salt']),
-                 pii_hash(payload['body']['email'],
-                          payload['body']['pii_salt']),
-                 unlock_after_dt)
-            )
+            if locked_info['success']:
+
+                unlock_after_dt = (datetime.utcnow() +
+                                   timedelta(seconds=unlock_after_seconds))
+
+                # schedule the unlock
+                loop.call_later(
+                    unlock_after_seconds,
+                    self.scheduled_user_unlock,
+                    user_info['user_info']['user_id'],
+                    payload['body']['reqid'],
+                    payload['body']['pii_salt']
+                )
+
+                LOGGER.warning(
+                    "Locked the account for user ID: %s, "
+                    "email: %s after repeated "
+                    "failed login attempts. "
+                    "Unlock scheduled for: %sZ" %
+                    (pii_hash(user_info['user_info']['user_id'],
+                              payload['body']['pii_salt']),
+                     pii_hash(payload['body']['email'],
+                              payload['body']['pii_salt']),
+                     unlock_after_dt)
+                )
 
         # we'll return a failure here as the response no matter what happens
         # above to deny the login
         return {
             "success": False,
-            "reqid": payload['reqid'],
+            "reqid": payload['body']['reqid'],
             "response":{
                 "success":False,
                 "user_id":None,
@@ -388,7 +393,7 @@ class AuthHandler(tornado.web.RequestHandler):
         '''
 
         loop = tornado.ioloop.IOLoop.current()
-        locked_info = await loop.run_executor(
+        locked_info = await loop.run_in_executor(
             actions.internal_toggle_user_lock,
             {'target_userid':user_id,
              'action':'unlock',
