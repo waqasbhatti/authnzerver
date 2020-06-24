03cea6ed9a538b4a85001c4e7b42efc2162a23ec
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2020-06-24 02:00:57 -0400

    quick fix for typos

--- authnzerver/tests/test_server.py
+++ authnzerver/tests/test_server.py
@@ -12,14 +12,16 @@ import os.path
 import time
 from datetime import datetime, timedelta
 
-from pytest.mark import skipif
+import pytest.mark
 
 from authnzerver.autosetup import autogen_secrets_authdb
 from authnzerver.messaging import encrypt_message, decrypt_message
 
 
-@skipif(os.environ.get("GITHUB_WORKFLOW",
-                       reason="github doesn't allow server tests probably"))
+@pytest.mark.skipif(
+    os.environ.get("GITHUB_WORKFLOW",
+                   reason="github doesn't allow server tests probably")
+)
 def test_server_with_env(monkeypatch, tmpdir):
     '''
     This tests if the server starts fine with all config in the environment.
@@ -164,8 +166,10 @@ def test_server_with_env(monkeypatch, tmpdir):
         )
 
 
-@skipif(os.environ.get("GITHUB_WORKFLOW",
-                       reason="github doesn't allow server tests probably"))
+@pytest.mark.skipif(
+    os.environ.get("GITHUB_WORKFLOW",
+                   reason="github doesn't allow server tests probably")
+)
 def test_server_invalid_logins(monkeypatch, tmpdir):
     '''This tests if the server responds appropriately to invalid logins.
 
@@ -392,8 +396,10 @@ def test_server_invalid_logins(monkeypatch, tmpdir):
         )
 
 
-@skipif(os.environ.get("GITHUB_WORKFLOW",
-                       reason="github doesn't allow server tests probably"))
+@pytest.mark.skipif(
+    os.environ.get("GITHUB_WORKFLOW",
+                   reason="github doesn't allow server tests probably")
+)
 def test_server_invalid_logins_with_lock(monkeypatch, tmpdir):
     '''This tests if the server responds appropriately to invalid logins.
 
