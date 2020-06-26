460c577f539f1b71214eb2defa27ebd1462ee68e
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2020-06-26 13:38:44 -0400

    tests: add backoff test for failed 'user-passcheck-nosession' reqs

--- authnzerver/tests/test_server.py
+++ authnzerver/tests/test_server.py
@@ -635,3 +635,239 @@ def test_server_invalid_logins_with_lock(monkeypatch, tmpdir):
             "lsof | grep 18158 | awk '{ print $2 }' | sort | uniq | xargs kill",
             shell=True
         )
+
+
+@mark.skipif(
+    os.environ.get("GITHUB_WORKFLOW", None) is not None,
+    reason="github doesn't allow server tests probably"
+)
+def test_server_invalid_passchecks_with_lock(monkeypatch, tmpdir):
+    '''This tests if the server responds appropriately to
+    invalid password checks.
+
+    The timing difference between successive failed password checks should
+    increase roughly exponentially. In addition, the user should be locked out
+    of their account for the configured amount of time and unlocked thereafter.
+
+    '''
+
+    # the basedir will be the pytest provided temporary directory
+    basedir = str(tmpdir)
+
+    # we'll make the auth DB and secrets file first
+    authdb_path, creds, secrets_file, salt_file, env_file = (
+        autogen_secrets_authdb(
+            basedir,
+            interactive=False
+        )
+    )
+
+    # read in the secrets file for the secret
+    with open(secrets_file,'r') as infd:
+        secret = infd.read().strip('\n')
+
+    # read in the salts file for the salt
+    with open(salt_file,'r') as infd:
+        salt = infd.read().strip('\n')
+
+    # read the creds file so we can try logging in
+    with open(creds,'r') as infd:
+        useremail, password = infd.read().strip('\n').split()
+
+    # get a temp directory
+    tmpdir = os.path.join('/tmp', 'authnzrv-%s' % secrets.token_urlsafe(8))
+
+    server_listen = '127.0.0.1'
+    server_port = '18158'
+
+    # set up the environment
+    monkeypatch.setenv("AUTHNZERVER_AUTHDB", authdb_path)
+    monkeypatch.setenv("AUTHNZERVER_BASEDIR", basedir)
+    monkeypatch.setenv("AUTHNZERVER_CACHEDIR", tmpdir)
+    monkeypatch.setenv("AUTHNZERVER_DEBUGMODE", "0")
+    monkeypatch.setenv("AUTHNZERVER_LISTEN", server_listen)
+    monkeypatch.setenv("AUTHNZERVER_PORT", server_port)
+    monkeypatch.setenv("AUTHNZERVER_SECRET", secret)
+    monkeypatch.setenv("AUTHNZERVER_PIISALT", salt)
+    monkeypatch.setenv("AUTHNZERVER_SESSIONEXPIRY", "60")
+    monkeypatch.setenv("AUTHNZERVER_WORKERS", "1")
+    monkeypatch.setenv("AUTHNZERVER_EMAILSERVER", "smtp.test.org")
+    monkeypatch.setenv("AUTHNZERVER_EMAILPORT", "25")
+    monkeypatch.setenv("AUTHNZERVER_EMAILUSER", "testuser")
+    monkeypatch.setenv("AUTHNZERVER_EMAILPASS", "testpass")
+    monkeypatch.setenv("AUTHNZERVER_USERLOCKTRIES", "2")
+    monkeypatch.setenv("AUTHNZERVER_USERLOCKTIME", "20")
+
+    # launch the server subprocess
+    p = subprocess.Popen("authnzrv", shell=True)
+
+    # wait 2.5 seconds for the server to start
+    time.sleep(2.5)
+
+    timing = []
+
+    try:
+
+        #
+        # attempt to login as the superuser several times with the wrong
+        # password
+        #
+        for i in range(4):
+
+            # create a new anonymous session token
+            session_payload = {
+                'user_id':2,
+                'user_agent':'Mozzarella Killerwhale',
+                'expires':datetime.utcnow()+timedelta(hours=1),
+                'ip_address': '1.1.1.1',
+                'extra_info_json':{'pref_datasets_always_private':True}
+            }
+
+            request_dict = {'request':'session-new',
+                            'body':session_payload,
+                            'reqid':i}
+
+            encrypted_request = encrypt_message(request_dict, secret)
+
+            # send the request to the authnzerver
+            resp = requests.post(
+                'http://%s:%s' % (server_listen, server_port),
+                data=encrypted_request,
+                timeout=1.0
+            )
+            resp.raise_for_status()
+
+            # decrypt the response
+            session_dict = decrypt_message(resp.text, secret)
+
+            assert session_dict['reqid'] == request_dict['reqid']
+            assert session_dict['success'] is True
+            assert isinstance(session_dict['response'], dict)
+            assert session_dict['response']['session_token'] is not None
+
+            request_dict = {
+                'request':'user-passcheck-nosession',
+                'body':{
+                    'email':useremail,
+                    'password':'%s-%i' % (password,i)
+                },
+                'reqid':10*i + 10
+            }
+
+            encrypted_request = encrypt_message(request_dict, secret)
+
+            start_login_time = time.monotonic()
+
+            # send the request to the authnzerver
+            resp = requests.post(
+                'http://%s:%s' % (server_listen, server_port),
+                data=encrypted_request,
+                timeout=60.0
+            )
+            resp.raise_for_status()
+
+            timing.append(time.monotonic() - start_login_time)
+
+            # decrypt the response
+            response_dict = decrypt_message(resp.text, secret)
+
+            assert response_dict['reqid'] == request_dict['reqid']
+            assert response_dict['success'] is False
+            assert isinstance(response_dict['response'], dict)
+            assert response_dict['response']['user_id'] is None
+
+            # for the last attempt, we should get back a "locked" account
+            # message
+            if i >= 2:
+
+                assert (
+                    "Your user account has been locked "
+                    "after repeated login failures. "
+                    "Try again in an hour or "
+                    "contact the server admins."
+                ) in response_dict['messages']
+
+        # wait 30 seconds for the lock time to expire
+        time.sleep(30)
+
+        # now login wih the correct password and see if we can login now
+        session_payload = {
+            'user_id':2,
+            'user_agent':'Mozzarella Killerwhale',
+            'expires':datetime.utcnow()+timedelta(hours=1),
+            'ip_address': '1.1.1.1',
+            'extra_info_json':{'pref_datasets_always_private':True}
+        }
+
+        request_dict = {'request':'session-new',
+                        'body':session_payload,
+                        'reqid':1004}
+
+        encrypted_request = encrypt_message(request_dict, secret)
+
+        # send the request to the authnzerver
+        resp = requests.post(
+            'http://%s:%s' % (server_listen, server_port),
+            data=encrypted_request,
+            timeout=1.0
+        )
+        resp.raise_for_status()
+
+        # decrypt the response
+        session_dict = decrypt_message(resp.text, secret)
+
+        assert session_dict['reqid'] == request_dict['reqid']
+        assert session_dict['success'] is True
+        assert isinstance(session_dict['response'], dict)
+        assert session_dict['response']['session_token'] is not None
+
+        request_dict = {
+            'request':'user-passcheck-nosession',
+            'body':{
+                'email':useremail,
+                'password':password
+            },
+            'reqid':1005
+        }
+
+        encrypted_request = encrypt_message(request_dict, secret)
+
+        start_login_time = time.monotonic()
+
+        # send the request to the authnzerver
+        resp = requests.post(
+            'http://%s:%s' % (server_listen, server_port),
+            data=encrypted_request,
+            timeout=60.0
+        )
+        resp.raise_for_status()
+
+        timing.append(time.monotonic() - start_login_time)
+
+        # decrypt the response
+        response_dict = decrypt_message(resp.text, secret)
+
+        assert response_dict['reqid'] == request_dict['reqid']
+        assert response_dict['success'] is True
+        assert isinstance(response_dict['response'], dict)
+        assert response_dict['response']['user_id'] == 1
+
+    finally:
+
+        #
+        # kill the server at the end
+        #
+
+        p.kill()
+        try:
+            p.communicate(timeout=1.0)
+            p.kill()
+        except Exception:
+            pass
+
+        # make sure to kill authnzrv on some Linux machines.  use lsof and the
+        # port number to find the remaining authnzrv processes and kill them
+        subprocess.call(
+            "lsof | grep 18158 | awk '{ print $2 }' | sort | uniq | xargs kill",
+            shell=True
+        )
