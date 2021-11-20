e8e1e2f9059a93bc6ad8bb5d08c10605f13bbb62
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2021-11-20 15:01:24 -0500

    permissions: blacken, add types

--- authnzerver/permissions.py
+++ authnzerver/permissions.py
@@ -12,6 +12,7 @@ This contains the permissions and user-role models for authnzerver.
 #############
 
 import logging
+from typing import Union, Set
 
 # get a logger
 LOGGER = logging.getLogger(__name__)
@@ -28,6 +29,7 @@ from hashlib import sha256, blake2b
 ## FUNCTIONS TO HASH PII FOR LOGS ##
 ####################################
 
+
 def pii_sha256_hash(item, salt):
     """This generates a SHA256 hash for a PII-item that is concatenated with a
     random 'salt' to add some security against attacks, but still allow
@@ -35,9 +37,7 @@ def pii_sha256_hash(item, salt):
 
     """
 
-    return sha256(
-        ("%s%s" % (item, salt)).encode('utf-8')
-    ).hexdigest()[:24]
+    return sha256(("%s%s" % (item, salt)).encode("utf-8")).hexdigest()[:24]
 
 
 def pii_blake2b_hash(item, key):
@@ -46,10 +46,10 @@ def pii_blake2b_hash(item, key):
 
     """
 
-    hasher = blake2b(key=key.encode('utf-8'),
-                     digest_size=12,
-                     person=b'authnzrv-pii')
-    hasher.update(str(item).encode('utf-8'))
+    hasher = blake2b(
+        key=key.encode("utf-8"), digest_size=12, person=b"authnzrv-pii"
+    )
+    hasher.update(str(item).encode("utf-8"))
     return hasher.hexdigest()
 
 
@@ -70,45 +70,46 @@ def pii_hash(item, key):
 ##  PERMISSION MODEL FUNCTIONS ##
 #################################
 
-def load_permissions_json(model_json):
+
+def load_permissions_json(model_json: str) -> dict:
     """Loads a permissions JSON and returns the model."""
 
-    with open(model_json, 'r') as infd:
+    with open(model_json, "r") as infd:
         model = json.load(infd)
 
     # load the item policy
-    item_policy = model['item_policy']
+    item_policy = model["item_policy"]
 
     for itemkey in item_policy:
-        item_policy[itemkey]['valid_role_actions'] = set(
-            item_policy[itemkey]['valid_role_actions']
+        item_policy[itemkey]["valid_role_actions"] = set(
+            item_policy[itemkey]["valid_role_actions"]
         )
-        item_policy[itemkey]['valid_visibilities'] = set(
-            item_policy[itemkey]['valid_visibilities']
+        item_policy[itemkey]["valid_visibilities"] = set(
+            item_policy[itemkey]["valid_visibilities"]
         )
-        item_policy[itemkey]['invalid_roles'] = set(
-            item_policy[itemkey]['invalid_roles']
+        item_policy[itemkey]["invalid_roles"] = set(
+            item_policy[itemkey]["invalid_roles"]
         )
 
     # load the role policy
-    role_policy = model['role_policy']
+    role_policy = model["role_policy"]
 
     for rolekey in role_policy:
-        role_policy[rolekey]['can_own_items'] = set(
-            role_policy[rolekey]['can_own_items']
+        role_policy[rolekey]["can_own_items"] = set(
+            role_policy[rolekey]["can_own_items"]
         )
-        role_policy[rolekey]['allowed_actions_for_owned'] = set(
-            role_policy[rolekey]['allowed_actions_for_owned']
+        role_policy[rolekey]["allowed_actions_for_owned"] = set(
+            role_policy[rolekey]["allowed_actions_for_owned"]
         )
-        for actionkey in role_policy[rolekey]['allowed_actions_for_other']:
-            role_policy[rolekey]['allowed_actions_for_other'][actionkey] = set(
-                role_policy[rolekey]['allowed_actions_for_other'][actionkey]
+        for actionkey in role_policy[rolekey]["allowed_actions_for_other"]:
+            role_policy[rolekey]["allowed_actions_for_other"][actionkey] = set(
+                role_policy[rolekey]["allowed_actions_for_other"][actionkey]
             )
 
-    model['roles'] = set(model['roles'])
-    model['items'] = set(model['items'])
-    model['actions'] = set(model['actions'])
-    model['visibilities'] = set(model['visibilities'])
+    model["roles"] = set(model["roles"])
+    model["items"] = set(model["items"])
+    model["actions"] = set(model["actions"])
+    model["visibilities"] = set(model["visibilities"])
 
     return model
 
@@ -117,12 +118,15 @@ def load_permissions_json(model_json):
 ## CHECKING PERMISSIONS ##
 ##########################
 
-def get_item_actions(permissions_model,
-                     role_name,
-                     target_name,
-                     target_visibility,
-                     target_ownership,
-                     debug=False):
+
+def get_item_actions(
+    permissions_model: dict,
+    role_name: str,
+    target_name: str,
+    target_visibility: str,
+    target_ownership: str,
+    debug: bool = False,
+) -> Set:
     """Returns the possible actions for a target given a role and target
     status.
 
@@ -160,34 +164,30 @@ def get_item_actions(permissions_model,
 
     """
 
-    role_policy = permissions_model['role_policy']
-    item_policy = permissions_model['item_policy']
+    role_policy = permissions_model["role_policy"]
+    item_policy = permissions_model["item_policy"]
 
     if debug:
         print(
-            'role_name = %s\ntarget_name = %s\n'
-            'target_visibility = %s\ntarget_ownership = %s' %
-            (role_name, target_name, target_visibility, target_ownership)
+            "role_name = %s\ntarget_name = %s\n"
+            "target_visibility = %s\ntarget_ownership = %s"
+            % (role_name, target_name, target_visibility, target_ownership)
         )
 
     try:
-        target_valid_actions = item_policy[
-            target_name
-        ]['valid_role_actions']
-        target_valid_visibilities = item_policy[
-            target_name
-        ]['valid_visibilities']
-        target_invalid_roles = item_policy[
-            target_name
-        ]['invalid_roles']
+        target_valid_actions = item_policy[target_name]["valid_role_actions"]
+        target_valid_visibilities = item_policy[target_name][
+            "valid_visibilities"
+        ]
+        target_invalid_roles = item_policy[target_name]["invalid_roles"]
 
         if debug:
-            print('%s valid_perms: %r' %
-                  (target_name, target_valid_actions))
-            print('%s valid_visibilities: %r' %
-                  (target_name, target_valid_visibilities))
-            print('%s invalid_roles: %r' %
-                  (target_name, target_invalid_roles))
+            print("%s valid_perms: %r" % (target_name, target_valid_actions))
+            print(
+                "%s valid_visibilities: %r"
+                % (target_name, target_valid_visibilities)
+            )
+            print("%s invalid_roles: %r" % (target_name, target_invalid_roles))
 
         # if the role is not allowed into this target, return
         if role_name in target_invalid_roles:
@@ -201,26 +201,22 @@ def get_item_actions(permissions_model,
 
         # if this target is owned by the user, then check target owned
         # actions
-        if target_ownership == 'for_owned':
-            role_actions = role_policy[role_name]['allowed_actions_for_owned']
+        if target_ownership == "for_owned":
+            role_actions = role_policy[role_name]["allowed_actions_for_owned"]
 
         # otherwise, the target is not owned by the user (target_ownership ==
         # 'for_other'), check ownership actions for target status
         else:
-            role_actions = (
-                role_policy[role_name]['allowed_actions_for_other'][
-                    target_visibility
-                ]
-            )
+            role_actions = role_policy[role_name]["allowed_actions_for_other"][
+                target_visibility
+            ]
 
         # these are the final available actions
-        available_actions = role_actions.intersection(
-            target_valid_actions
-        )
+        available_actions = role_actions.intersection(target_valid_actions)
 
         if debug:
             print("target role actions: %r" % role_actions)
-            print('available actions for role: %r' % available_actions)
+            print("available actions for role: %r" % available_actions)
 
         return available_actions
 
@@ -228,15 +224,17 @@ def get_item_actions(permissions_model,
         return set({})
 
 
-def check_item_access(permissions_model,
-                      userid=2,
-                      role='anonymous',
-                      action='view',
-                      target_name='collection',
-                      target_owner=1,
-                      target_visibility='private',
-                      target_sharedwith=None,
-                      debug=False):
+def check_item_access(
+    permissions_model: dict,
+    userid: int = 2,
+    role: str = "anonymous",
+    action: str = "view",
+    target_name: str = "collection",
+    target_owner: int = 1,
+    target_visibility: str = "private",
+    target_sharedwith: str = None,
+    debug: bool = False,
+) -> bool:
     """
     This does a check for user access to a target item.
 
@@ -279,32 +277,36 @@ def check_item_access(permissions_model,
 
     """
 
-    role_policy = permissions_model['role_policy']
+    role_policy = permissions_model["role_policy"]
 
     if debug:
-        print('userid = %s\ntarget_owner = %s\nsharedwith_userids = %s' %
-              (userid, target_owner, target_sharedwith))
+        print(
+            "userid = %s\ntarget_owner = %s\nsharedwith_userids = %s"
+            % (userid, target_owner, target_sharedwith)
+        )
 
-    if role in ('superuser', 'staff'):
+    if role in ("superuser", "staff"):
 
         shared_or_owned_ok = True
 
-    elif target_visibility == 'private':
+    elif target_visibility == "private":
 
         shared_or_owned_ok = userid == target_owner
 
-    elif target_visibility == 'shared':
+    elif target_visibility == "shared":
 
         try:
 
-            if (target_sharedwith and
-                target_sharedwith != '' and
-                target_sharedwith.lower() != 'none'):
+            if (
+                target_sharedwith
+                and target_sharedwith != ""
+                and target_sharedwith.lower() != "none"
+            ):
 
-                sharedwith_userids = target_sharedwith.split(',')
+                sharedwith_userids = target_sharedwith.split(",")
                 sharedwith_userids = [int(x) for x in sharedwith_userids]
                 if debug:
-                    print('sharedwith_userids = %s' % sharedwith_userids)
+                    print("sharedwith_userids = %s" % sharedwith_userids)
 
                 shared_or_owned_ok = (
                     userid in sharedwith_userids or userid == target_owner
@@ -316,19 +318,17 @@ def check_item_access(permissions_model,
                     shared_or_owned_ok = True
 
             else:
-                shared_or_owned_ok = (
-                    userid == target_owner
-                )
+                shared_or_owned_ok = userid == target_owner
 
         except Exception:
             shared_or_owned_ok = False
 
     # unlisted objects are OK to view
-    elif target_visibility == 'unlisted':
+    elif target_visibility == "unlisted":
 
         shared_or_owned_ok = True
 
-    elif target_visibility == 'public':
+    elif target_visibility == "public":
 
         shared_or_owned_ok = True
 
@@ -337,34 +337,40 @@ def check_item_access(permissions_model,
         shared_or_owned_ok = False
 
     if debug:
-        print('target shared or owned test passed = %s' % shared_or_owned_ok)
+        print("target shared or owned test passed = %s" % shared_or_owned_ok)
 
     target_may_be_owned_by_role = (
-        target_name in role_policy[role]['can_own_items']
+        target_name in role_policy[role]["can_own_items"]
     )
 
     if debug:
-        print("target: '%s' may be owned by role: '%s' = %s" %
-              (target_name, role, target_may_be_owned_by_role))
+        print(
+            "target: '%s' may be owned by role: '%s' = %s"
+            % (target_name, role, target_may_be_owned_by_role)
+        )
 
     # validate ownership of the target
     if userid == target_owner and target_may_be_owned_by_role:
-        perms = get_item_actions(permissions_model,
-                                 role,
-                                 target_name,
-                                 target_visibility,
-                                 'for_owned',
-                                 debug=debug)
+        perms = get_item_actions(
+            permissions_model,
+            role,
+            target_name,
+            target_visibility,
+            "for_owned",
+            debug=debug,
+        )
 
     # if the target is not owned, then check if it's accessible under its scope
     # and visibility
     elif userid != target_owner:
-        perms = get_item_actions(permissions_model,
-                                 role,
-                                 target_name,
-                                 target_visibility,
-                                 'for_other',
-                                 debug=debug)
+        perms = get_item_actions(
+            permissions_model,
+            role,
+            target_name,
+            target_visibility,
+            "for_other",
+            debug=debug,
+        )
 
     # if the target cannot be owned by the role, then fail
     else:
@@ -377,15 +383,16 @@ def check_item_access(permissions_model,
 
 
 def load_policy_and_check_access(
-        permissions_json,
-        userid=2,
-        role='anonymous',
-        action='view',
-        target_name='collection',
-        target_owner=1,
-        target_visibility='private',
-        target_sharedwith=None,
-        debug=False):
+    permissions_json: str,
+    userid: int = 2,
+    role: str = "anonymous",
+    action: str = "view",
+    target_name: str = "collection",
+    target_owner: int = 1,
+    target_visibility: str = "private",
+    target_sharedwith: str = None,
+    debug: bool = False,
+) -> bool:
     """
     Does a check for user access to a target item.
 
@@ -439,14 +446,17 @@ def load_policy_and_check_access(
         target_name=target_name,
         target_owner=target_owner,
         target_visibility=target_visibility,
-        target_sharedwith=target_sharedwith
+        target_sharedwith=target_sharedwith,
+        debug=debug,
     )
 
 
-def check_role_limits(permissions_model,
-                      role,
-                      limit_name,
-                      value_to_check):
+def check_role_limits(
+    permissions_model: dict,
+    role: str,
+    limit_name: str,
+    value_to_check: Union[float, int],
+) -> bool:
     """
     This applies the role limits to a value to check.
 
@@ -473,10 +483,10 @@ def check_role_limits(permissions_model,
 
     """
 
-    role_policy = permissions_model['role_policy']
-    limit_defs = permissions_model['limits']
+    role_policy = permissions_model["role_policy"]
+    limit_defs = permissions_model["limits"]
 
-    all_role_limits = role_policy[role]['limits'].get(limit_name)
+    all_role_limits = role_policy[role]["limits"].get(limit_name)
 
     # if there's no limit for the requested type, return True to indicate this
     if not all_role_limits:
@@ -491,24 +501,26 @@ def check_role_limits(permissions_model,
 
     # cast the value to the appropriate type
     try:
-        if limit_vartype == 'int':
+        if limit_vartype == "int":
             value_to_check = int(value_to_check)
             limit_to_apply = int(limit_to_apply)
 
-        elif limit_vartype == 'float':
+        elif limit_vartype == "float":
             value_to_check = float(value_to_check)
             limit_to_apply = float(limit_to_apply)
 
     except Exception:
-        LOGGER.error("Could not convert value to be checked "
-                     "to expected type for limit operator")
+        LOGGER.error(
+            "Could not convert value to be checked "
+            "to expected type for limit operator"
+        )
         return False
 
     # parse the operator and apply the limit
     try:
-        if limit_operator == 'gt':
+        if limit_operator == "gt":
             return value_to_check > limit_to_apply
-        elif limit_operator == 'lt':
+        elif limit_operator == "lt":
             return value_to_check < limit_to_apply
         else:
             LOGGER.error("unknown operator requested for limit")
@@ -520,10 +532,10 @@ def check_role_limits(permissions_model,
 
 
 def load_policy_and_check_limits(
-        permissions_json,
-        role,
-        limit_name,
-        value_to_check
+    permissions_json: str,
+    role: str,
+    limit_name: str,
+    value_to_check: Union[float, int],
 ):
     """
     Applies the role limits to a value to check.
@@ -555,8 +567,5 @@ def load_policy_and_check_limits(
 
     permissions_model = load_permissions_json(permissions_json)
     return check_role_limits(
-        permissions_model,
-        role,
-        limit_name,
-        value_to_check
+        permissions_model, role, limit_name, value_to_check
     )
