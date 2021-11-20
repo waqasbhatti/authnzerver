82e7cc479f900dc200782a88df9cab043618250e
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2021-11-20 15:00:56 -0500

    actions: add utils.py for authdb retrieval from currproc

---
+++ authnzerver/actions/utils.py
@@ -0,0 +1,45 @@
+"""
+Contains utilities for actions.
+
+"""
+
+from multiprocessing import current_process
+from typing import Tuple
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import MetaData
+
+from authnzerver import authdb
+
+
+def get_procdb_permjson(
+    override_authdb_path: str = None,
+    override_permissions_json: str = None,
+    raiseonfail: bool = False,
+) -> Tuple[Engine, MetaData, str]:
+    """
+    Gets the DB and permissions JSON path in the current process namespace.
+
+    If these don't exist, will put the db in the current process's namespace and
+    return it.
+
+    """
+
+    currproc = current_process()
+
+    if override_authdb_path:
+        currproc.auth_db_path = override_authdb_path
+
+    if override_permissions_json:
+        currproc.permissions_json = override_permissions_json
+
+    if not getattr(currproc, "authdb_engine", None):
+        (currproc.authdb_engine, currproc.authdb_meta,) = authdb.get_auth_db(
+            currproc.auth_db_path, echo=raiseonfail, returnconn=False
+        )
+
+    return (
+        currproc.authdb_engine,
+        currproc.authdb_meta,
+        currproc.permissions_json,
+    )
