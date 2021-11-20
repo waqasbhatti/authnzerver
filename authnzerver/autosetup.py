1a0db0d7c88c3181789df06a968295e07a871378
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2021-11-20 15:17:01 -0500

    autosetup: blacken

--- authnzerver/autosetup.py
+++ authnzerver/autosetup.py
@@ -32,10 +32,9 @@ from .modtools import object_from_string
 modpath = os.path.abspath(os.path.dirname(__file__))
 
 
-def autogen_secrets_authdb(basedir,
-                           database_url=None,
-                           interactive=False,
-                           generate_envfile=True):
+def autogen_secrets_authdb(
+    basedir, database_url=None, interactive=False, generate_envfile=True
+):
     """This automatically generates secrets files and an authentication DB.
 
     Run this only once on the first start of an authnzerver.
@@ -89,7 +88,9 @@ def autogen_secrets_authdb(basedir,
 
     import getpass
     from .authdb import (
-        create_sqlite_authdb, initial_authdb_inserts, create_authdb
+        create_sqlite_authdb,
+        initial_authdb_inserts,
+        create_authdb,
     )
     from cryptography.fernet import Fernet
 
@@ -98,7 +99,7 @@ def autogen_secrets_authdb(basedir,
     #
     mod_dir = os.path.dirname(__file__)
     permissions_json = os.path.abspath(
-        os.path.join(mod_dir, 'default-permissions-model.json')
+        os.path.join(mod_dir, "default-permissions-model.json")
     )
 
     if interactive:
@@ -111,9 +112,7 @@ def autogen_secrets_authdb(basedir,
         )
         print("If you leave this blank and hit Enter, an SQLite auth DB")
         print("will be created in the base directory: %s" % basedir)
-        input_db_url = input(
-            "Auth DB URL [default: auto generated]: "
-        )
+        input_db_url = input("Auth DB URL [default: auto generated]: ")
         if not input_db_url or len(input_db_url) == 0:
             database_url = None
 
@@ -121,8 +120,10 @@ def autogen_secrets_authdb(basedir,
             "Enter the path to the permissions policy JSON file to use."
         )
         print("If you leave this blank and hit Enter, the default permissions")
-        print("policy JSON shipped with authnzerver will be used: %s" %
-              permissions_json)
+        print(
+            "policy JSON shipped with authnzerver will be used: %s"
+            % permissions_json
+        )
         input_permissions_json = input(
             "Permission JSON path [default: included permissions JSON]: "
         )
@@ -131,20 +132,22 @@ def autogen_secrets_authdb(basedir,
 
     # if no database_url is specified, create our auth DB in the basedir
     if database_url is None:
-        authdb_path = os.path.join(basedir, '.authdb.sqlite')
+        authdb_path = os.path.join(basedir, ".authdb.sqlite")
         if not os.path.exists(authdb_path):
-            LOGGER.warning('No existing authentication DB was found, '
-                           'making a new SQLite DB in authnzerver basedir: %s'
-                           % authdb_path)
+            LOGGER.warning(
+                "No existing authentication DB was found, "
+                "making a new SQLite DB in authnzerver basedir: %s"
+                % authdb_path
+            )
 
         # generate the initial DB
         create_sqlite_authdb(authdb_path, echo=False, returnconn=False)
-        database_url = 'sqlite:///%s' % authdb_path
+        database_url = "sqlite:///%s" % authdb_path
 
     # otherwise, if there's an SQLite DB URL provided,
     # create it at the specified path
-    elif database_url is not None and 'sqlite:///' in database_url:
-        authdb_path = os.path.abspath(database_url.replace('sqlite:///', ''))
+    elif database_url is not None and "sqlite:///" in database_url:
+        authdb_path = os.path.abspath(database_url.replace("sqlite:///", ""))
         if not os.path.exists(authdb_path):
             create_sqlite_authdb(authdb_path, echo=False, returnconn=False)
 
@@ -158,21 +161,18 @@ def autogen_secrets_authdb(basedir,
     # is None, we'll use the user's UNIX ID@localhost if the password is
     # None, a random one will be generated
     try:
-        userid = '%s@localhost' % getpass.getuser()
+        userid = "%s@localhost" % getpass.getuser()
     except Exception:
-        userid = 'serveradmin@localhost'
+        userid = "serveradmin@localhost"
 
     if interactive:
 
-        inp_userid = input(
-            '\nAdmin email address [default: %s]: ' %
-            userid
-        )
+        inp_userid = input("\nAdmin email address [default: %s]: " % userid)
         if inp_userid and len(inp_userid.strip()) > 0:
             userid = inp_userid
 
         inp_pass = getpass.getpass(
-            'Admin password [default: randomly generated]: '
+            "Admin password [default: randomly generated]: "
         )
         if inp_pass and len(inp_pass.strip()) > 0:
             password = inp_pass
@@ -187,10 +187,12 @@ def autogen_secrets_authdb(basedir,
     try:
 
         # generate the admin users and initial DB info
-        u, p = initial_authdb_inserts(database_url,
-                                      permissions_json=permissions_json,
-                                      superuser_email=userid,
-                                      superuser_pass=password)
+        u, p = initial_authdb_inserts(
+            database_url,
+            permissions_json=permissions_json,
+            superuser_email=userid,
+            superuser_pass=password,
+        )
 
         if u is None:
             LOGGER.error("Could not do initial inserts into the auth DB.")
@@ -203,83 +205,78 @@ def autogen_secrets_authdb(basedir,
             "Not overwriting..."
         )
 
-    creds = os.path.join(basedir, '.authnzerver-admin-credentials')
+    creds = os.path.join(basedir, ".authnzerver-admin-credentials")
 
     if os.path.exists(creds):
-        LOGGER.warning("Admin credentials file already exists. "
-                       "Not overwriting...")
+        LOGGER.warning(
+            "Admin credentials file already exists. " "Not overwriting..."
+        )
 
     elif u and p:
-        with open(creds, 'w') as outfd:
-            outfd.write('%s %s\n' % (u, p))
+        with open(creds, "w") as outfd:
+            outfd.write("%s %s\n" % (u, p))
             os.chmod(creds, 0o100400)
 
         if p:
-            LOGGER.warning('Generated random admin password, '
-                           'credentials written to: %s\n' %
-                           creds)
+            LOGGER.warning(
+                "Generated random admin password, "
+                "credentials written to: %s\n" % creds
+            )
 
     # we'll generate the server secrets now so we don't have to deal
     # with them later
-    LOGGER.info('Generating server secret tokens...')
+    LOGGER.info("Generating server secret tokens...")
     fernet_secret = Fernet.generate_key()
-    fernet_secret_file = os.path.join(basedir, '.authnzerver-secret-key')
+    fernet_secret_file = os.path.join(basedir, ".authnzerver-secret-key")
 
     if os.path.exists(fernet_secret_file):
 
-        LOGGER.warning("Authnzerver communication secrets file already "
-                       "exists. Not overwriting...")
+        LOGGER.warning(
+            "Authnzerver communication secrets file already "
+            "exists. Not overwriting..."
+        )
 
     else:
 
-        with open(fernet_secret_file, 'wb') as outfd:
+        with open(fernet_secret_file, "wb") as outfd:
             outfd.write(fernet_secret)
         os.chmod(fernet_secret_file, 0o100400)
 
     # finally, we'll generate the server PII random salt
-    LOGGER.info('Generating server PII random salt...')
+    LOGGER.info("Generating server PII random salt...")
     salt = Fernet.generate_key()
-    salt_file = os.path.join(basedir, '.authnzerver-salt')
+    salt_file = os.path.join(basedir, ".authnzerver-salt")
 
     if os.path.exists(salt_file):
 
-        LOGGER.warning("Authnzerver salt file already "
-                       "exists. Not overwriting...")
+        LOGGER.warning(
+            "Authnzerver salt file already " "exists. Not overwriting..."
+        )
 
     else:
 
-        with open(salt_file, 'wb') as outfd:
+        with open(salt_file, "wb") as outfd:
             outfd.write(salt)
         os.chmod(salt_file, 0o100400)
 
     # copy over the permission model and confvars
     LOGGER.info(
-        "Copying default-permissions-model.json to basedir: %s" %
-        basedir
+        "Copying default-permissions-model.json to basedir: %s" % basedir
     )
     shutil.copy(
-        os.path.join(modpath, 'default-permissions-model.json'),
-        basedir
+        os.path.join(modpath, "default-permissions-model.json"), basedir
     )
-    LOGGER.info(
-        "Copying confvars.py to basedir: %s" %
-        basedir
-    )
-    shutil.copy(
-        os.path.join(modpath, 'confvars.py'),
-        basedir
-    )
+    LOGGER.info("Copying confvars.py to basedir: %s" % basedir)
+    shutil.copy(os.path.join(modpath, "confvars.py"), basedir)
 
     # generate the env file if asked for
     if generate_envfile:
         LOGGER.info(
-            "Generating an envfile: %s" %
-            os.path.join(basedir, '.env')
+            "Generating an envfile: %s" % os.path.join(basedir, ".env")
         )
 
         envfile = generate_env(
             database_url if database_url is not None else authdb_path,
-            creds,
             fernet_secret_file,
             salt_file,
             basedir,
@@ -287,7 +284,6 @@ def autogen_secrets_authdb(basedir,
     else:
         envfile = generate_env(
             database_url,
-            creds,
             fernet_secret_file,
             salt_file,
             basedir,
@@ -298,24 +294,12 @@ def autogen_secrets_authdb(basedir,
     #
 
     if database_url is not None:
-        return (database_url,
-                creds,
-                fernet_secret_file,
-                salt_file,
-                envfile)
+        return database_url, creds, fernet_secret_file, salt_file, envfile
     else:
-        return (authdb_path,
-                creds,
-                fernet_secret_file,
-                salt_file,
-                envfile)
+        return authdb_path, creds, fernet_secret_file, salt_file, envfile
 
 
-def generate_env(database_path,
-                 creds,
-                 fernet_secret_file,
-                 salt_file,
-                 basedir):
+def generate_env(database_path, fernet_secret_file, salt_file, basedir):
     """This generates environment variables containing the required items for
     authnzrv start up after autosetup is complete.
 
@@ -330,9 +314,6 @@ def generate_env(database_path,
         SQLite database. If ``database_path`` points to a file on disk, this
         function will assume it's an SQLite file and construct the appropriate
         SQLAlchemy database URL.
-
-    creds : str
-        The path to the admin credentials file.
 
     fernet_secret_file : str
         The path to the shared secret key needed to secure authnzerver-frontend
@@ -356,8 +337,8 @@ def generate_env(database_path,
 
     # first, figure out the database URL
     if os.path.exists(database_path):
-        database_url = 'sqlite:///%s' % os.path.abspath(database_path)
-    elif '://' in database_path:
+        database_url = "sqlite:///%s" % os.path.abspath(database_path)
+    elif "://" in database_path:
         database_url = database_path
     else:
         LOGGER.error("Could not understand the database_path provided.")
@@ -365,37 +346,37 @@ def generate_env(database_path,
 
     # get the confvar.py file and generate the env variables in it
     confvars = object_from_string(
-        '%s::CONF' % os.path.join(basedir, 'confvars.py')
+        "%s::CONF" % os.path.join(basedir, "confvars.py")
     )
 
-    env_file = os.path.abspath(os.path.join(basedir, '.env'))
+    env_file = os.path.abspath(os.path.join(basedir, ".env"))
 
-    with open(env_file, 'w') as outfd:
+    with open(env_file, "w") as outfd:
 
         for key, val in confvars.items():
 
-            if key == 'authdb':
-                env_key, env_val = val['env'], database_url
+            if key == "authdb":
+                env_key, env_val = val["env"], database_url
 
-            elif key == 'secret':
-                env_key, env_val = (val['env'],
-                                    os.path.abspath(fernet_secret_file))
-
-            elif key == 'piisalt':
-                env_key, env_val = (val['env'],
-                                    os.path.abspath(salt_file))
-
-            elif key == 'permissions':
+            elif key == "secret":
                 env_key, env_val = (
-                    val['env'],
+                    val["env"],
+                    os.path.abspath(fernet_secret_file),
+                )
+
+            elif key == "piisalt":
+                env_key, env_val = (val["env"], os.path.abspath(salt_file))
+
+            elif key == "permissions":
+                env_key, env_val = (
+                    val["env"],
                     os.path.abspath(
-                        os.path.join(basedir,
-                                     'default-permissions-model.json')
-                    )
+                        os.path.join(basedir, "default-permissions-model.json")
+                    ),
                 )
 
             else:
-                env_key, env_val = val['env'], val['default']
+                env_key, env_val = val["env"], val["default"]
 
             # handle multiple env keys by assigning them all to the value
             if isinstance(env_key, (list, tuple)):
