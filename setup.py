d3bcdd101225f58264a44ede2588c3539547c30b
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2021-11-20 16:02:57 -0500

    setup.py: blacken

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 
-'''This sets up the package.
+"""This sets up the package.
 
-Stolen from http://python-packaging.readthedocs.io/en/latest/everything.html and
-modified by me.
+Stolen from https://python-packaging.readthedocs.io/en/latest/everything.html
+and modified by me.
 
-'''
+"""
 
 import versioneer
+
 __version__ = versioneer.get_version()
 
 from setuptools import setup, find_packages
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
 
 # let's be lazy and put requirements in one place
 # what could possibly go wrong?
-with open('requirements.txt') as infd:
-    INSTALL_REQUIRES = [x.strip('\n') for x in infd.readlines()]
+with open("requirements.txt") as infd:
+    INSTALL_REQUIRES = [x.strip("\n") for x in infd.readlines()]
 
 cmdclass = versioneer.get_cmdclass()
 
@@ -30,15 +31,15 @@ cmdclass = versioneer.get_cmdclass()
 ###############
 
 setup(
-    name='authnzerver',
+    name="authnzerver",
     version=__version__,
     cmdclass=cmdclass,
-    description=('A small authentication-authorization server.'),
+    description="A small authentication-authorization server.",
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP :: Session",
         "Operating System :: OS Independent",
@@ -48,20 +49,20 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    keywords='authentication authorization tornado',
-    url='https://github.com/waqasbhatti/authnzerver',
-    author='Waqas Bhatti',
-    author_email='waqas.afzal.bhatti@gmail.com',
-    license='MIT',
+    keywords="authentication authorization tornado",
+    url="https://github.com/waqasbhatti/authnzerver",
+    author="Waqas Bhatti",
+    author_email="waqas.afzal.bhatti@gmail.com",
+    license="MIT",
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     entry_points={
-        'console_scripts':[
-            'authnzrv=authnzerver.main:main',
-            'authnzrv-frontend=authnzerver.frontend.main:main',
+        "console_scripts": [
+            "authnzrv=authnzerver.main:main",
+            "authnzrv-frontend=authnzerver.frontend.main:main",
         ],
     },
     include_package_data=True,
     zip_safe=False,
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
